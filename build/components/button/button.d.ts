9f81a57d94b89a96d465fbbc861ee23ab2b07622
Russell Maxfield <russellmaxfield@russells-mbp.lan>
2020-08-26 10:16:33 -0600

    react import

--- build/components/button/button.d.ts
+++ build/components/button/button.d.ts
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 interface BaseButtonProps {
     size?: 'tiny' | 'small' | 'normal' | 'large' | 'huge';
     fullWidth?: boolean;
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,5 @@
+import React from 'react'
 import styled, { css } from 'styled-components'
-import * as React from 'react'
 import { Color } from '../../global/default-theme'
 
 interface BaseButtonProps {
